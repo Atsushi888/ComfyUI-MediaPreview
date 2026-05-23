ad4be8d202b669bbb8a610c883eef47d4c4b8152
Atsushi888 <vtgrotxu@gmail.com>
2026-05-23 15:07:55 +0000

    Add version info

--- __init__.py
+++ __init__.py
@@ -1,11 +1,17 @@
+from .config import MEDIA_PREVIEW_VERSION
 from .node import MediaPreview
+
+_VERSION_SUFFIX = f"v{MEDIA_PREVIEW_VERSION}"
+
+print(f"[MediaPreview] {_VERSION_SUFFIX }")
 
 WEB_DIRECTORY = "web"
 
 NODE_CLASS_MAPPINGS = {
     "MediaPreview": MediaPreview,
 }
-
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "MediaPreview": "Media Preview",
+    "MediaPreview": (
+        f"Media Preview({ _VERSION_SUFFIX })"
+    )
 }
